80d380316b55808a91965714cfb5fab3c6acfb4e
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-05-08 11:31:52 +0700

    Add favicon

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Standard Betting'
+    return render_template('index.html', title="Home")
 
 @app.route('/<game>/<id>')
 def result(game, id):
